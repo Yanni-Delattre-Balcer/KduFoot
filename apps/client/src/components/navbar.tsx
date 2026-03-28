ee0d22b0969c0ffede2ba5cb4d5f82551270dd2b
Delattre Balcer <yanni_delattrebalcer@ens.univ-artois.fr>
2026-03-28 01:56:16 +0100

    feat: Implement a new responsive Navbar component with navigation, language switching, authentication, and badge display for pending items.

--- apps/client/src/components/navbar.tsx
+++ apps/client/src/components/navbar.tsx
@@ -36,7 +36,6 @@ import { Chip } from "@heroui/chip";
 
 import { I18nIcon, LanguageSwitch } from "./language-switch";
 import { LinkUniversal } from "./link-universal";
-import { getCloudflareOptimizedUrl } from "@/utils/image";
 
 import {
   LoginLogoutButton,
@@ -137,7 +136,7 @@ export const Navbar = () => {
           <img
             alt="KduFoot Logo"
             className="h-14 lg:h-16 w-auto object-contain"
-            src={getCloudflareOptimizedUrl("/logo.png", { width: 120 })}
+            src="/logo.png"
           />
         </a>
       </div>
